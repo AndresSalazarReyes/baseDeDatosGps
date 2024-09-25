10c06c82d7b1da04e15f21287b800b093659904e
TatianaCS05 <tatianacortes197@gmail.com>
2024-09-25 17:29:16 -0500

    rama dev

--- proyecto/main.js
+++ proyecto/main.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config(); // Cargar variables de entorno desde el archivo .env
+
 const app = express();
 const authRoutes = require('./routes/authRoutes'); 
 const activosRoutes = require('./routes/activosRoutes'); 
@@ -6,9 +9,14 @@ const activosRoutes = require('./routes/activosRoutes');
 app.use(express.json());  
 
 app.use('/auth', authRoutes);
-
 app.use('/activos', activosRoutes);
 
 app.listen(3000, () => {
     console.log('Servidor corriendo en el puerto 3000');
+
+    if (!process.env.JWT_SECRET) {
+        console.error('Error: JWT_SECRET no está definido. Verifica tu archivo .env y reinicia el servidor.');
+      } else {
+        console.log('JWT_SECRET cargado correctamente');
+      }
 });
